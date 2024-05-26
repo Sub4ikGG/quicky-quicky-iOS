0a46b5fe58ef5ade440f85d7910c38b755108cb0
@KEfremov <kirill.efremov@re-coders.pro>
2024-05-26 16:11:33 +0300

    Initial Commit

---
+++ quickyquicky/quickyquickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickyquickyApp.swift
+//  quickyquicky
+//
+//  Created by Kirill Efremov on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct quickyquickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
